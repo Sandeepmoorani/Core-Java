b39a2237db39714b37003464ea8c89cd49356755
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-07-31 05:47:57 -0700

    Add files via upload
    first commit

---
+++ first.java
@@ -0,0 +1,14 @@
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+    }
+}
+
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin git@github.com:Sandeepmoorani/Core-Java.git
+// git push -u origin main
