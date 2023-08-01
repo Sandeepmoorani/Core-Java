51cac74887d10cba0931e8a6534032699e3a21c5
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-01 10:42:28 -0700

    commit init

---
+++ LogicalOP.java
@@ -0,0 +1,29 @@
+public class LogicalOP {
+    public static void main(String[] args) {
+        
+
+        int a= 5;
+        int b =6;
+        int c = 7;
+        int d =8;
+
+        // boolean result = a<b && b>a;
+        // boolean result= a>b || c<d;
+		// boolean result= a>b && c>d ;
+		// boolean result= a>b|| c>d  ;
+		// boolean result= a<b && c< d ;
+//		boolean result= x<y || a<b ;
+		boolean result= a<b || c<d || a>1 ;
+        // System.out.println(result);
+
+        // boolean result= a>b ;
+		System.out.println(!result);
+        
+
+    
+
+    }
+    
+}
+
+		
