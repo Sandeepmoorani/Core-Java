d0857a5b1eda0040a207eb8618bb5fbd9205f71d
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-01 08:58:15 -0700

    second commit

--- var_datatypes.java
+++ var_datatypes.java
@@ -1,10 +1,33 @@
 public class var_datatypes {
     public static void main(String[] args) {
+
+
+        // variables are the container which stores the data 
         
         int a = 8;
         int b=10;
         int c = a+b;
         System.out.println(c);
+
+
+        // datatypes 
+        // there are two types of datatyes 
+        // 1)  primitive 
+        // 2) non_permitive 
+
+        // byte int short long float double and so on 
+
+        // int stores intiger value 
+        int a1 =10;
+
+        // short also store intiger value but limit 
+        short s = 127;
+
+        // boolean in boolean only two thing true or false 
+        boolean bo = true;
+
+
+        
     }
     
 }
