fc96cfa26c297cf9c95b2baa99a78272d025834d
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-01 12:04:39 -0700

    commit added

--- if_else_if.java
+++ if_else_if.java
@@ -1,34 +1,45 @@
 public class if_else_if {
     public static void main(String[] args) {
-        int x=8;
-		int y=9;
-		int z=5;
-//		
-// 		if(x>y && x>z)  
-// 		{
-// 			System.out.println(x);
-// 		}
-// 		else
-// 			System.out.println(y);
-		
-		// if(x>y && x>z)         
-		// 	System.out.println(x);
-		// else if(y>x && y>z)
-		// 	System.out.println(y);
-		// else
-		// 	System.out.println(z);
+        // int x=8;
+        // int y=9;
+        // int z=5;
+        //
+        // if(x>y && x>z)
+        // {
+        // System.out.println(x);
+        // }
+        // else
+        // System.out.println(y);
 
+        // if(x>y && x>z)
+        // System.out.println(x);
+        // else if(y>x && y>z)
+        // System.out.println(y);
+        // else
+        // System.out.println(z);
 
-       
-		if(x>y && x>z)         
-			System.out.println(x);
-		else if(y>z)
-			System.out.println(y);
-		else
-			System.out.println(z);
-		
-		
-		
+        // if(x>y && x>z)
+        // System.out.println(x);
+        // else if(y>z)
+        // System.out.println(y);
+        // else
+        // System.out.println(z);
+
+        // ternry operator
+
+        // int n = 4;
+        // int result = 0;
+        // if (n % 2 == 0)
+        //     result = 10;
+        // else
+        //     result = 20;
+        // System.out.println(result);
+
+        int n=5;
+        int result=0;
+        result = n%2==0 ? 10 : 20;
+        System.out.println(result);
+
     }
-    
+
 }
