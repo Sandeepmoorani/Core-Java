cd55aed8d19f43e8072140c332b19fdb5c989240
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-01 11:52:51 -0700

    commit init

---
+++ ifelse.java
@@ -0,0 +1,19 @@
+public class ifelse {
+    public static void main(String[] args) {
+        int x = 10;
+        // int result =0;
+        // if(true)
+        // System.out.println("Yes");
+
+        // if(x==11)
+        // System.out.println("true");
+        // else
+        // System.out.println("false");
+
+        if( x>20 && x<=12)
+        System.out.println("yes");
+        else
+        System.out.println("false");
+    }
+    
+}
