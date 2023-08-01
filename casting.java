711ae0c596cd2b8525066ed76ed1b83e7d3c96c3
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-01 09:15:36 -0700

    commit init

---
+++ casting.java
@@ -0,0 +1,36 @@
+public class casting {
+    public static void main(String[] args) {
+       ;
+
+        byte b= 12;
+        int a= b;
+        System.out.println(b);
+        System.out.println(a);
+		
+		byte b1=125;
+		int a0=b1;
+		System.out.println(b1);
+		System.out.println(a0);
+		
+		int aa=257;
+		byte k=(byte)aa;
+		
+		float f=5.6f;
+		int t=(int)f;
+		
+		int a2=2567;
+		byte b2=(byte)a;
+		System.out.println(k);
+		
+		byte a3=10;
+		byte b4=20;
+		int t1=a*b;
+		System.out.println(t1);
+
+
+        int n =23;
+        byte m =(byte)n;
+        System.out.println(m);
+    }
+    
+}
