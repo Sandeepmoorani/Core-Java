0d8f58acb54a9117349a8e820807d583d281309c
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-01 08:51:34 -0700

    commit init

---
+++ var_datatypes.java
@@ -0,0 +1,10 @@
+public class var_datatypes {
+    public static void main(String[] args) {
+        
+        int a = 8;
+        int b=10;
+        int c = a+b;
+        System.out.println(c);
+    }
+    
+}
