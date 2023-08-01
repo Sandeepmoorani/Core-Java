a930e0679e471d0ea5d1296342c4e521dbab0e52
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-01 10:20:38 -0700

    commit made

---
+++ assignmentop.java
@@ -0,0 +1,40 @@
+public class assignmentop {
+    public static void main(String[] args) {
+        
+
+// Additions of two numbers 
+        // int a = 1;
+        // int b =2; 
+        // int c = a+b;
+        // System.out.println(c);
+
+// Substraction 
+
+        //  int a = 1;
+        // int b =2; 
+        // int c = b-a;
+        // System.out.println(c);
+
+// Multiplication of two numbers 
+
+        // int a = 1;
+        // int b =2; 
+        // int c = a*b;
+        // System.out.println(c);
+
+// Division of two numbers 
+
+        // int a = 1;
+        // int b =2; 
+        // int c = b/a;
+        // System.out.println(c);
+
+// modulo operator 
+         
+        int a = 1;
+        int b =2; 
+        int c = a%b;
+        System.out.println(c);
+    }
+    
+}
