ed65bd82a7ab92fe8cd12db9026406f9e39dbe6c
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-01 11:59:00 -0700

    commit init

---
+++ if_else_if.java
@@ -0,0 +1,25 @@
+public class if_else_if {
+    public static void main(String[] args) {
+        int x=8;
+		int y=7;
+		int z=9;
+//		
+// 		if(x>y && x>z)  
+// 		{
+// 			System.out.println(x);
+// 		}
+// 		else
+// 			System.out.println(y);
+		
+		if(x>y && x>z)         
+			System.out.println(x);
+		else if(y>x && y>z)
+			System.out.println(y);
+		else
+			System.out.println(z);
+		
+		
+		
+    }
+    
+}
