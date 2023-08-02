8e131bf84602f3d2d73fa07c472253c727f1f8eb
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-02 09:29:18 -0700

    commit init

---
+++ while_loop.java
@@ -0,0 +1,18 @@
+public class while_loop {
+    public static void main(String[] args) {
+        int x =1;
+        while(x<=5){
+            System.out.println("hey java how are you"+x);
+
+            int y =1;
+            while(y<=1){
+                System.out.println("iam fine! you say?"+y);
+                y++;
+            }
+            x++;
+          
+        }
+        System.out.println(x);
+    }
+    
+}
