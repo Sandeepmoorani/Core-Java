afdb58c2b37cf13e373df408d8373a68d4134042
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-02 11:31:44 -0700

    commit init

---
+++ for_loop.java
@@ -0,0 +1,9 @@
+public class for_loop {
+    public static void main(String[] args) {
+        
+        for (int i=1; i<=12; i++)
+        System.out.println("Month" + " " + i);
+       
+    }
+    
+}
