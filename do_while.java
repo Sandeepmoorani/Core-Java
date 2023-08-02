272cd625e9e94c75ffe5e759b1b92efbd2ff5739
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-02 10:40:19 -0700

    commit made

---
+++ do_while.java
@@ -0,0 +1,17 @@
+public class do_while {
+    public static void main(String[] args) {
+        int x= 9;
+
+        do{
+            System.out.println("hey java print value of x" +x);
+
+        }
+
+        while(x<=5);
+        {
+            System.out.println("hello java"+ x);
+            x++;
+        }
+    }
+    
+}
