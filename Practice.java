1cbd94746e42867b6f2b3ae95943f6c4b9e05bcd
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-03 04:10:26 -0700

    first commit

---
+++ Practice.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Practice {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        
+        int a = scan.nextInt();
+        
+       
+        // scan.close();
+
+        System.out.println("myString is :" + str);
+        System.out.println("myInt is:"+ a);
+    }
+
+    
+}
