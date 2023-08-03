7e2d9e856400700af27dfb7ea020b8d55744789f
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-03 04:18:56 -0700

    second commit

--- Practice.java
+++ Practice.java
@@ -2,16 +2,28 @@ import java.util.*;
 
 public class Practice {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String str = scan.next();
+        // Scanner scan = new Scanner(System.in);
+        // String str = scan.next();
         
-        int a = scan.nextInt();
+        // int a = scan.nextInt();
         
        
-        // scan.close();
+        // // scan.close();
 
-        System.out.println("myString is :" + str);
-        System.out.println("myInt is:"+ a);
+        // System.out.println("myString is :" + str);
+        // System.out.println("myInt is:"+ a);
+
+
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("enter your first intiger");
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println(" your first intiger:" +a);
+        System.out.println("your second intiger:" +b);
+        System.out.println("your third intiger:" +c);
     }
 
     
