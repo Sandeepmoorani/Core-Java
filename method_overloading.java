f885d1ea8bfcf1a25bf8316d18585ce443c63c2a
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-04 14:15:37 -0700

    commit init

---
+++ method_overloading.java
@@ -0,0 +1,34 @@
+class calculator{
+
+    // public int add(int num1, int num2){
+    //     return num1+num2;
+    // }
+
+    // public int add(int num1, int num2, int num3){
+    //     return num1+num2+num3;
+    // }
+
+    // public double add(double num1 ,int num2){
+    //     return num1+num2;
+    // }
+}
+public class method_overloading {
+    public static void main(String[] args) {
+        
+        // method_overloading mo = new method_overloading();
+        // int a = mo.add(1,2);
+        // System.out.println(a);
+
+
+        // int b = mo.add(1,2,3);
+        // System.out.println(b);
+        // int c = mo.add(1,2,3);
+        // System.out.println(c);
+
+
+        
+    }
+
+  
+    
+}
