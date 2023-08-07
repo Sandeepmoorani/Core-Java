29024e3723af0ee73fcd4705e538d5c85fda19c0
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-07 11:17:36 -0700

    added

--- conditionals.java
+++ conditionals.java
@@ -3,3 +3,16 @@
 if(condition) {    
 statement 1; //executes when condition is true   
 }  
+
+//  IF_ELSE  statement
+// The if-else statement is an extension to the if-statement, which uses another block of code, i.e., else block.
+// The else block is executed if the condition of the if-block is evaluated as false.
+
+// Syntax:
+
+if(condition) {    
+statement 1; //executes when condition is true   
+}  
+else{  
+statement 2; //executes when condition is false   
+}  
