c2891e33637c1550bf293f54add1341c07607966
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-13 22:14:14 -0700

    committed

--- Array.java
+++ Array.java
@@ -12,17 +12,36 @@ public class Array {
 
         // another way of creating an array 
 
-        int arr[] = new int[5];
+        // int arr[] = new int[5];
 
-        arr[0]=1;
-        arr[0]=2;
-        arr[0]=3;
-        arr[0]=4;
-        arr[0]=5;
+        // arr[0]=1;
+        // arr[1]=2;
+        // arr[2]=3;
+        // arr[3]=4;
+        // arr[4]=5;
 
-        for ( int i=0; i<5; i++){
-            System.out.println(arr[i]);
-        }
+        // for ( int i=0; i<5; i++){
+        //     System.out.println(arr[i]);
+        // }
+
+
+        int nums[]=new int[4];
+		nums[0]=4;
+		nums[1]=8;
+		nums[2]=3;
+		nums[3]=9;
+		
+//		System.out.println(nums[0]);
+//		System.out.println(nums[1]);
+//		System.out.println(nums[2]);
+//		System.out.println(nums[3]);
+		
+		for (int i=0;i<4;i++) {
+			System.out.println(nums[i]);
+		}
+
+
+        
     }
     
 }
