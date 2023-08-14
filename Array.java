91289c73a5bae4b4a088fe6de142e43d1012a4e7
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-13 22:10:55 -0700

    commit added

--- Array.java
+++ Array.java
@@ -2,9 +2,27 @@ public class Array {
     public static void main(String[] args) {
         
 
-        int arr[]= {9,8,7,6};
+        // int arr[]= {9,8,7,6};
 
-        System.out.println(arr[2]);
+        // you also change the vale like 
+
+        // arr[1] = 2;
+
+        // System.out.println(arr[3]);
+
+        // another way of creating an array 
+
+        int arr[] = new int[5];
+
+        arr[0]=1;
+        arr[0]=2;
+        arr[0]=3;
+        arr[0]=4;
+        arr[0]=5;
+
+        for ( int i=0; i<5; i++){
+            System.out.println(arr[i]);
+        }
     }
     
 }
