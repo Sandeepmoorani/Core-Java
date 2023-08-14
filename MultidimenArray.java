6d9c2b7583a18b71e5b4a192195f5ec6d64a52e4
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-13 22:35:55 -0700

    commit made

--- MultidimenArray.java
+++ MultidimenArray.java
@@ -1,6 +1,17 @@
 public class MultidimenArray {
     public static void main(String[] args) {
         
+        int arr1[][] = new int[4][5];
+
+        for(int i=0; i<4;i++){
+            for(int j =0; j<5; j++){
+
+                System.out.print(arr1[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+
         int arr[][] = new int[4][4];
 
         for(int i = 0; i<4; i++){
