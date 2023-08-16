fd00149812108f6d737448af269e1f0259689f6e
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-16 10:48:34 -0700

    updated

--- Array.java
+++ Array.java
@@ -45,3 +45,10 @@ public class Array {
     }
     
 }
+
+
+// Drawbacks:-
+// - The memory allocation is contiguous.
+// - The size of an array is fixed. Array size cannot be expand.
+// - Searching takes time.
+// - Array can store values of only same type. It can store homogeneous type value only.
