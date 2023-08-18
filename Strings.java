8e4a588541ebf23e143973a1c63f5a3c0f58cfc1
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-18 04:43:25 -0700

    updated

--- Strings.java
+++ Strings.java
@@ -2,11 +2,29 @@ public class Strings {
     public static void main(String[] args) {
         // two ways to create a string in java // 1. String literal
 		
-        String name1=new String("Sandeep");
+       /*  String name1=new String("Sandeep");
         System.out.println(name1);
 
         String name2="Sandeep";
-        System.out.println(name2);
+        System.out.println(name2); */
+
+
+
+        String st[] = new String[5];
+        st[0]="Sandeep";
+        st[1]="qurban";
+        st[2]="zahoor";
+        st[3]="sandeep";
+        st[4]="qurban";
+
+        for(String n : st){
+            System.out.println(n);
+        }
+
+
+
+
+    
 
 
         
