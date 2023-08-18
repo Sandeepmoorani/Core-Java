15231d2109e8d6ee993159633b06a2141d1c5e7f
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-18 04:39:06 -0700

    commit init

---
+++ Strings.java
@@ -0,0 +1,15 @@
+public class Strings {
+    public static void main(String[] args) {
+        // two ways to create a string in java // 1. String literal
+		
+        String name1=new String("Sandeep");
+        System.out.println(name1);
+
+        String name2="Sandeep";
+        System.out.println(name2);
+
+
+        
+    }
+    
+}
