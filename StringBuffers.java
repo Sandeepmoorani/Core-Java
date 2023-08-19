83e77591f121816bab26e6a9963839b83e5c49b2
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-19 01:56:11 -0700

    commit init

---
+++ StringBuffers.java
@@ -0,0 +1,23 @@
+public class StringBuffers {
+    public static void main(String[] args) {
+
+        // what is string buffer?
+        // String buffer is a class in java which is used to create mutable strings
+        // mutable means changeable
+        
+
+
+        
+        String StringBuffers = new String();
+
+        StringBuffers.concat("Sandeep");
+        StringBuffers.concat("Moorani");
+
+        System.out.println(StringBuffers);
+        // System.out.println(StringBuffers.length());
+        // System.out.println(StringBuffers.charAt(0));
+        // System.out.println(StringBuffers.charAt(1));
+
+    }
+    
+}
