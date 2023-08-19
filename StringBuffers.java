469520881ec6288b52c694e7fe38c615c65e2618
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-08-19 02:06:53 -0700

    aded

--- StringBuffers.java
+++ StringBuffers.java
@@ -5,18 +5,34 @@ public class StringBuffers {
         // String buffer is a class in java which is used to create mutable strings
         // mutable means changeable
         
+        // what is difference between string and string buffer?
+        // string is immutable and string buffer is mutable
+
+        // how to create string buffer?
+        // there are two ways to create string buffer
+        // 1. using string buffer class
+        // 2. using string builder class
+
+        // 1. using string buffer class
+        StringBuffer sb = new StringBuffer("Sandeep");
+        System.out.println(sb);
+
+        // 2. using string builder class
+
+        StringBuilder sb1 = new StringBuilder("Sandeep");
+        System.out.println(sb1);
+
+        // string buffer methods
+
+        // 1. append()
+        sb.append("Moorani");
+        System.out.println(sb);
+
+
 
 
         
-        String StringBuffers = new String();
-
-        StringBuffers.concat("Sandeep");
-        StringBuffers.concat("Moorani");
-
-        System.out.println(StringBuffers);
-        // System.out.println(StringBuffers.length());
-        // System.out.println(StringBuffers.charAt(0));
-        // System.out.println(StringBuffers.charAt(1));
+        
 
     }
     
